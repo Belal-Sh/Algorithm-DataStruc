007291225ca8cbf4adabc6bcb3e707b7a725e2b6
Belal-Sh <belal.she7adeh@outlook.com>
2021-08-08 23:32:01 +0300

    solution added!

--- reversestring/index.js
+++ reversestring/index.js
@@ -7,7 +7,28 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
+  // Sol-3
+debugger;
+return str.split('').reduce((reversed,char) => char + reversed,'')
 }
 
+reverse("belal")
+}
 
 module.exports = reverse;
+
+// Sol-2
+//   let reversed = '';
+//   for (let char of str) {
+  //     reversed = char + reversed
+  //   }
+  //   return reversed;
+  // }
+
+
+  // Sol-1
+  // let arr = str.split('');
+  // arr.reverse();
+  // let rstr = arr.join('');
+  // return rstr;
+  // return str.split('').reverse().join('');
