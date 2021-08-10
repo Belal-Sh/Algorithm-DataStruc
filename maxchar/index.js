ce81f67ff655a3d444b367379ee7fedc2285e431
Belal-Sh <belal.she7adeh@outlook.com>
2021-08-10 20:13:19 +0300

    solved!

--- maxchar/index.js
+++ maxchar/index.js
@@ -6,7 +6,16 @@
 // maxChar("apple 1231111") === "1"
 
 function maxChar(str) {
-
+  // sol-1
+    let chars = {};
+    let max = 0;
+    for (let char of str) {
+        (!chars[char])?chars[char] =1 : chars[char]++;
+    }
+    for (let char of str) {
+    (chars[char]>max) ? max = chars[char]:"";
+    }
+   return Object.keys(chars).find((key) => chars[key]===max)
 }
 
 module.exports = maxChar;
